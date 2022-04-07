cb2f27bdf67fa6b7728591c753aed69b6533f0e3
Keshav Parpyani <86668000+Dopeman55@users.noreply.github.com>
2022-04-08 00:30:13 +0530

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,34 @@
+var urlsToCache = [
+    '/',
+    '/style.css',
+    '/main.js',
+    '/index.html'
+   
+  ];
+  self.addEventListener('install', (event) => {
+    console.log("service worker installted")
+    event.waitUntil(
+      caches.open('static')
+        .then((cache) => {
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
+  self.addEventListener('activate', event => {
+    console.log("service worker is register")
+  });
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => {
+          // The responce is in the cache
+          if (response) {
+            return response;
+          }
+          // No cache match, we attempt to fetch it from the network
+          return fetch(event.request);
+        }
+        )
+    );
+  });
+  
